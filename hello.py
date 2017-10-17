27bd72bb7f501fe0be6dcf17d06417892f0c750d
T A <arvta@paypalcorp.com>
2017-10-17 15:52:30 +0530

    starter flask app

---
+++ hello.py
@@ -0,0 +1,55 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+import os
+app = Flask(__name__)
+app.debug = True
+
+# serving static files from 'static folder'
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+
+
+@app.route("/")
+def index():
+  return render_template('index.html')
+
+
+@app.route("/hello/<name>")
+def hello(name):
+  return '<h1>Hello, World! %s </h1>' % name
+
+# handle success login
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+# handle login post , get
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+
+# handle form submit
+@app.route('/result', methods=['POST'])
+def postResult():
+      # render the result in the html page
+      return render_template('successResult.html', result = request.form)
+ 
+
+
+# carry forward query params to the new url (hello.html from form submit)
+@app.route('/result', methods=['GET'])
+def getFormData():
+      return app.send_static_file('hello.html')
+     
+
+if __name__ == "__main__":
+  app.run()
+
+
